852c4caaa0e52ea3ea9789f89f1172f676cd5d97
Landon Bouma <landonb@users.noreply.github.com>
2023-12-27 20:21:17 -0600

    Bugfix: Repair test for CI

--- tests/test_editor.py
+++ tests/test_editor.py
@@ -7,7 +7,13 @@ def test_editor(capfd):
     cont = editor.edit(contents="ABC!", use_tty="use_tty" in sys.argv)
     sys.stdout.write(cont.decode())
     out, err = capfd.readouterr()
-    assert err == (
-        "Vim: Warning: Output is not to a terminal\n"
-        "Vim: Warning: Input is not from a terminal\n"
+    # I see two Warnings locally, and too many Warnings on GHA CI.
+    # - Though it might be my local EDITOR setting vs. CI's.
+    assert (
+        err
+        == (
+            "Vim: Warning: Output is not to a terminal\n"
+            "Vim: Warning: Input is not from a terminal\n"
+        )
+        or err == "Too many errors from stdin\n"
     )
