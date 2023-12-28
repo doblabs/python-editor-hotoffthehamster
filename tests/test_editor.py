6eab31780eac0a048de110318d9fa74fa01ad85e
Landon Bouma <landonb@users.noreply.github.com>
2023-12-27 20:14:38 -0600

    Tests: Wire existing test into pytest

--- tests/test_editor.py
+++ tests/test_editor.py
@@ -2,5 +2,12 @@ import sys
 
 import editor
 
-cont = editor.edit(contents="ABC!", use_tty="use_tty" in sys.argv)
-sys.stdout.write(cont.decode())
+
+def test_editor(capfd):
+    cont = editor.edit(contents="ABC!", use_tty="use_tty" in sys.argv)
+    sys.stdout.write(cont.decode())
+    out, err = capfd.readouterr()
+    assert err == (
+        "Vim: Warning: Output is not to a terminal\n"
+        "Vim: Warning: Input is not from a terminal\n"
+    )
